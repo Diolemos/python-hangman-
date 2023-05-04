fca4bf6263f787e7883dd4c0c86c3c03b63d2069
Dio Lemos <pedro.devsc@outlook.com>
2023-05-04 11:57:05 -0300

    fix typo in line 6. '-' is now '=' create for loop to check if user input is in picked word

--- index.py
+++ index.py
@@ -3,7 +3,14 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 
-picked_word - random.choice(word_list)
+picked_word = random.choice(word_list)
 
 
 guess= input("Guess a letter").lower()
+#how do I get the index? 
+for word in picked_word:
+    if word == guess:
+        print("Do something")
+    else:
+        print("do something else")    
+         
