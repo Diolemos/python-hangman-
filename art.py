4c7c7d1e463c46fb3bc3e3cfcf6541f86c7cabce
Dio Lemos <pedro.devsc@outlook.com>
2023-05-04 11:46:14 -0300

    first commit create module with art variable, functions and list

---
+++ art.py
@@ -0,0 +1,149 @@
+hangman  = """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+"""
+
+# def print_hangman1():
+#     print("  +---+")
+#     print("  |   |")
+#     print("      |")
+#     print("      |")
+#     print("      |")
+#     print("      |")
+#     print("=========")
+
+def print_hangman1():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print("      |")
+    print("      |")
+    print("      |")
+    print("=========")
+
+def print_hangman2():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print("  |   |")
+    print("      |")
+    print("      |")
+    print("=========")
+
+def print_hangman3():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print(" /|   |")
+    print("      |")
+    print("      |")
+    print("=========")
+
+def print_hangman4():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print(" /|\  |")
+    print("      |")
+    print("      |")
+    print("=========")
+
+def print_hangman5():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print(" /|\  |")
+    print(" /    |")
+    print("      |")
+    print("=========")
+
+def print_hangman6():
+    print("  +---+")
+    print("  |   |")
+    print("  O   |")
+    print(" /|\  |")
+    print(" / \  |")
+    print("      |")
+    print("=========")    
+    
+    
+#     hangman1 = """
+#   +---+
+#   |   |
+#       |
+#       |
+#       |
+#       |
+# =========
+# """
+
+
+
+
+hangman1 = """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+"""
+
+hangman2 = """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+"""
+
+hangman3 = """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+"""
+
+hangman4 = """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+"""
+
+hangman5 = """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+"""
+hangman6 = """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+"""
+
+
+# hangman_list = [hangman1,hangman2, hangman3, hangman4, hangman4, hangman5, hangman6]
+hangman_list = [print_hangman1,print_hangman2,print_hangman3,print_hangman4,print_hangman5,print_hangman6]
