5101604a478f95c920a0e6ee606959655ad9db8f
Dio Lemos <pedro.devsc@outlook.com>
2023-05-04 14:06:44 -0300

    add a more elegant solution to todo #2

--- index.py
+++ index.py
@@ -18,16 +18,23 @@ guess= input("Guess a letter").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-indexes = []
-# how do I get the index? 
-currentIndex = 0 
-for word in picked_word:
-    if word == guess:
-        display[currentIndex]= guess
-        currentIndex +=1
+# indexes = []
+# # how do I get the index? 
+# currentIndex = 0 
+# for word in picked_word:
+#     if word == guess:
+#         display[currentIndex]= guess
+#         currentIndex +=1
+#     else:
+#         currentIndex +=1
+for position in range(len(picked_word)):
+    letter = picked_word[position]
+    if letter == guess:
+        display[position]= guess
+        
     else:
-        currentIndex +=1
-           
+        print(".")    
+               
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.         
